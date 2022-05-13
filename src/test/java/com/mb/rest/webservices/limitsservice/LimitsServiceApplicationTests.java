3c2e83c891912a138fc12958898583ca78bd320f
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2022-05-13 10:32:27 -0400

    Initial commit

---
+++ src/test/java/com/mb/rest/webservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mb.rest.webservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
